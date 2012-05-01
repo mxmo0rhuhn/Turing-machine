20a9c82d0aba5169ddb1642ac316a6e6adb1545f
Reto Hablützel <rethab@rethab.ch>
2012-05-01 09:00:09 +0200

    genereller test

--- src.test/ch/zhaw/turing/logic/MultiplicationStateControlTest.java
+++ src.test/ch/zhaw/turing/logic/MultiplicationStateControlTest.java
@@ -11,75 +11,23 @@ import org.junit.Test;
  */ 
 public class MultiplicationStateControlTest {
 
+    private static final int ITERATIONEN = 20;    
+    
     @Test
-    public void zeroMultipliedByZero() {
-        MultiplicationStateControl curMultiplicationStateControl = new MultiplicationStateControl(0, 0);
-        
-        curMultiplicationStateControl.doAllSteps();
-        
-        Assert.assertEquals(0, curMultiplicationStateControl.getFirstNumberAsInteger());
+    public void sollRichtigMultiplizieren() {
+        for (int i = 0; i < ITERATIONEN; i++) {
+            Assert.assertTrue(multiplikationKorrekt(i, i + 1));
+            Assert.assertTrue(multiplikationKorrekt(i + 1, i));
+            Assert.assertTrue(multiplikationKorrekt(i, i));
+        }
     }
     
-    @Test
-    public void zeroMultipliedByFive() {
-        MultiplicationStateControl curMultiplicationStateControl = new MultiplicationStateControl(0, 5);
-        
+    private boolean multiplikationKorrekt(int a, int b) {
+        MultiplicationStateControl curMultiplicationStateControl = new MultiplicationStateControl(a, b);
         curMultiplicationStateControl.doAllSteps();
-        
-        Assert.assertEquals(0, curMultiplicationStateControl.getFirstNumberAsInteger());
+        int result = curMultiplicationStateControl.getFirstNumberAsInteger();
+        return result == a * b;
     }
-    
-    @Test
-    public void fiveMultipliedByZero() {
-        MultiplicationStateControl curMultiplicationStateControl = new MultiplicationStateControl(5, 0);
-        
-        curMultiplicationStateControl.doAllSteps();
-        
-        Assert.assertEquals(0, curMultiplicationStateControl.getFirstNumberAsInteger());
-    }
-    
-    @Test
-    public void oneMultipliedByOne() {
-        MultiplicationStateControl curMultiplicationStateControl = new MultiplicationStateControl(1, 1);
-        
-        curMultiplicationStateControl.doAllSteps();
-        
-        Assert.assertEquals(1, curMultiplicationStateControl.getFirstNumberAsInteger());
-    }
-    
-    @Test
-    public void twoMultipliedByTwo() {
-        MultiplicationStateControl curMultiplicationStateControl = new MultiplicationStateControl(2, 2);
-        
-        curMultiplicationStateControl.doAllSteps();
-        
-        Assert.assertEquals(4, curMultiplicationStateControl.getFirstNumberAsInteger());
-    }    
-    
-    @Test
-    public void fiveMultipliedByTwo() {
-        MultiplicationStateControl curMultiplicationStateControl = new MultiplicationStateControl(5, 2);
-        
-        curMultiplicationStateControl.doAllSteps();
-        
-        Assert.assertEquals(10, curMultiplicationStateControl.getFirstNumberAsInteger());
-    }    
 
-    @Test
-    public void twoMultipliedByFive() {
-        MultiplicationStateControl curMultiplicationStateControl = new MultiplicationStateControl(2, 5);
-        
-        curMultiplicationStateControl.doAllSteps();
-        
-        Assert.assertEquals(10, curMultiplicationStateControl.getFirstNumberAsInteger());
-    }    
-
-    @Test
-    public void fiveMultipliedByFive() {
-        MultiplicationStateControl curMultiplicationStateControl = new MultiplicationStateControl(5, 5);
-        
-        curMultiplicationStateControl.doAllSteps();
-        
-        Assert.assertEquals(25, curMultiplicationStateControl.getFirstNumberAsInteger());
-    }
+    
 }
