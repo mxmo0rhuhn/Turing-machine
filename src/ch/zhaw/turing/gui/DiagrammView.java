5ebf06aec89e77c8a9e980ee63f7e1ac119f528b
Reto Hablützel <rethab@rethab.ch>
2012-05-06 12:52:17 +0200

    uebergang fukntioniert

--- src/ch/zhaw/turing/gui/DiagrammView.java
+++ src/ch/zhaw/turing/gui/DiagrammView.java
@@ -1,12 +1,15 @@
 package ch.zhaw.turing.gui;
 
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 import java.util.Observable;
 import java.util.Observer;
 
-import javax.swing.ImageIcon;
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 import ch.zhaw.turing.logic.FactorialStateControl;
 import ch.zhaw.turing.logic.MultiplicationStateControl;
@@ -18,13 +21,17 @@ public class DiagrammView extends JFrame implements Observer {
 
     private static final String DIR_SEP = File.separator;
 
-    private static final String MULTIPLICATION_PATTERN = String.format("Multiplikation%sZustaende%s%s hervorgehoben.png", DIR_SEP, DIR_SEP, "%s");
+    private static final String MULTIPLICATION_PATTERN = String.format(
+            "Multiplikation%sZustaende%s%s hervorgehoben.png", DIR_SEP, DIR_SEP, "%s");
 
-    private static final String FACTORIAL_PATTERN = String.format("Fakultaet%sZustaende%s%s hervorgehoben.png", DIR_SEP, DIR_SEP, "%s");
-    
+    private static final String FACTORIAL_PATTERN = String.format("Fakultaet%sZustaende%s%s.png", DIR_SEP, DIR_SEP,
+            "%s");
+
     private File dir;
 
-    private final JLabel label = new JLabel();
+    private BufferedImage bufImage;
+
+    private final JPanel imgPanel = new JPanel();
 
     public DiagrammView() {
         super("Übergangsdiagramm");
@@ -32,10 +39,12 @@ public class DiagrammView extends JFrame implements Observer {
         if (!dir.exists()) {
             dir = new File(dir.getParent(), "Zeichnungen");
             if (!dir.exists()) {
-                throw new IllegalArgumentException("Wo sind die Zeichnungen? Habe sie in " + dir.getAbsolutePath() + " nicht gefunden..");
+                throw new IllegalArgumentException("Wo sind die Zeichnungen? Habe sie in " + dir.getAbsolutePath()
+                        + " nicht gefunden..");
             }
         }
-        pack();
+        getContentPane().add(imgPanel);
+        setSize(500, 500);
         setVisible(true);
     }
 
@@ -43,21 +52,34 @@ public class DiagrammView extends JFrame implements Observer {
     public void update(Observable o, Object arg) {
         TuringMachine m = (TuringMachine) o;
         String zustand = m.getCurrentState().toLowerCase();
-        
-        String imgPath;
+
+        String imgName;
         if (m instanceof FactorialStateControl) {
-            imgPath = String.format(FACTORIAL_PATTERN, zustand);
+            imgName = String.format(FACTORIAL_PATTERN, zustand);
         } else if (m instanceof MultiplicationStateControl) {
-            imgPath = String.format(MULTIPLICATION_PATTERN, zustand);
+            imgName = String.format(MULTIPLICATION_PATTERN, zustand);
         } else {
             throw new IllegalArgumentException(o.getClass() + " sollte nicht im DiagrammView landen..");
         }
-        
-        File img = new File(dir, imgPath);
-        if (!img.exists()) {
+
+        File imgPath = new File(dir, imgName);
+        if (!imgPath.exists()) {
             throw new IllegalArgumentException(imgPath + " existiert nicht..");
         }
-        
-        label.setIcon(new ImageIcon(img.getAbsolutePath()));
+
+        try {
+            this.bufImage = ImageIO.read(imgPath);
+            
+            repaint();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        if (bufImage != null) {
+            g.drawImage(bufImage, 0, 0, getWidth(), getHeight(), null);
+        }
     }
 }
